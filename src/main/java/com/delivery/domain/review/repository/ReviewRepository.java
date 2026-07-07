d20e9a045a33b51f6a4d074776289a7e971f198a
yonghyun0325 <127069161+yonghyun0325@users.noreply.github.com>
2026-07-07 15:46:46 +0900

    Revert "BaseEntity 및 JPA Auditing 설정 추가"

--- src/main/java/com/delivery/domain/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.delivery.domain.review.repository;
-
-import com.delivery.domain.review.entity.Review;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, UUID> {}
