eaf5850396907cfa6bb4ca182147376c05bfa3f2
cpfur18 <cpfur18@gmail.com>
2026-07-10 13:07:30 +0900

    feat: 테스트용 가짜 인증 객체 생성

--- src/test/java/com/delivery/testconfig/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/delivery/testconfig/WithMockCustomUserSecurityContextFactory.java
@@ -1,30 +1,34 @@
 package com.delivery.testconfig;
 
 import com.delivery.global.security.config.CustomUserDetails;
-import org.junit.platform.commons.util.StringUtils;
+import java.util.List;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
-import java.util.List;
-
-public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+public class WithMockCustomUserSecurityContextFactory
+        implements WithSecurityContextFactory<WithMockCustomUser> {
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser mockCustomUser) {
         final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
 
-        CustomUserDetails principal = CustomUserDetails.builder()
-                .id(mockCustomUser.id())
-                .username(mockCustomUser.userName())
-                .nickName(mockCustomUser.nickName())
-                .phoneNumber(mockCustomUser.phoneNumber())
-                .authorities(List.of(new SimpleGrantedAuthority("ROLE_" + mockCustomUser.role())))
-                .build();
+        CustomUserDetails principal =
+                CustomUserDetails.builder()
+                        .id(mockCustomUser.id())
+                        .username(mockCustomUser.userName())
+                        .nickName(mockCustomUser.nickName())
+                        .phoneNumber(mockCustomUser.phoneNumber())
+                        .authorities(
+                                List.of(
+                                        new SimpleGrantedAuthority(
+                                                "ROLE_" + mockCustomUser.role())))
+                        .build();
 
-        final UsernamePasswordAuthenticationToken authenticationToken
-                = new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+        final UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        principal, null, principal.getAuthorities());
 
         securityContext.setAuthentication(authenticationToken);
         return securityContext;
