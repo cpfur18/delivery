2fbaee49198ba6a702a66a31228f3fa842174a74
yonghyun0325 <dldydgus95426@gmail.com>
2026-07-14 14:10:50 +0900

    refactor: 리뷰 서비스 리팩토링 및 테스트 코드 작성

--- src/main/java/com/delivery/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/delivery/domain/review/repository/ReviewRepository.java
@@ -4,6 +4,10 @@ import com.delivery.domain.review.entity.Review;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.delivery.domain.review.enums.ReviewSortType;
+import com.delivery.domain.review.service.ReviewService;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +20,11 @@ public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
     List<Review> findAllByStoreIdAndDeletedAtIsNull(UUID storeId);
 
-    List<Review> findAllByUserIdAndDeletedAtIsNull(Long userId);
+    List<Review> findAllByStoreIdAndDeletedAtIsNull(UUID storeId, Sort sort);
+
+    List<Review> findAllByUserIdAndDeletedAtIsNull(Long userId, Sort latestSort);
+
+    boolean existsByOrderId(UUID orderId);
 
     @Query(
             """
