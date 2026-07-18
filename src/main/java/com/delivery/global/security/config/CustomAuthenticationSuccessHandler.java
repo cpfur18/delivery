bc9438330c5f2e7c5cbff59e65b7a19ef31a7e0f
cpfur18 <cpfur18@gmail.com>
2026-07-18 22:40:02 +0900

    feat: Security Login, Logout 성공 핸들러 추가

---
+++ src/main/java/com/delivery/global/security/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,55 @@
+package com.delivery.global.security.config;
+
+import com.delivery.common.RestApiResponse;
+import com.delivery.domain.user.dto.request.LoginRequest;
+import com.delivery.domain.user.dto.response.AuthResponse;
+import com.delivery.domain.user.service.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.delivery.global.config.JwtProperties.REFRESH_TOKEN_VALIDITY_SECONDS;
+
+@RequiredArgsConstructor
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler  {
+    private final ObjectMapper objectMapper;
+
+    private final AuthService authService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        AuthResponse loginResponse = authService.login(customUserDetails);
+
+        var cookie = ResponseCookie.from("refreshToken", loginResponse.refreshToken())
+                .maxAge(REFRESH_TOKEN_VALIDITY_SECONDS)
+                .path("/")
+                .secure(false)
+                .sameSite("Strict")
+                .httpOnly(true)
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        var authResponse =
+                RestApiResponse.success(
+                        HttpStatus.OK, "로그인 성공", loginResponse.accessToken());
+
+        objectMapper.writeValue(response.getWriter(), authResponse);
+    }
+}
---
+++ src/main/java/com/delivery/global/security/config/CustomLogoutSuccessHandler.java
@@ -0,0 +1,59 @@
+package com.delivery.global.security.config;
+
+import com.delivery.common.RestApiResponse;
+import com.delivery.domain.user.exception.AuthException;
+import com.delivery.domain.user.service.AuthService;
+import com.delivery.global.exception.BusinessException;
+import com.delivery.global.exception.ErrorCode;
+import com.delivery.global.exception.GlobalErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+    private final ObjectMapper objectMapper;
+    private final AuthService authService;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        RestApiResponse logoutResponse = null;
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.OK.value());
+
+        try {
+            authService.logout(request);
+            logoutResponse =
+                    RestApiResponse.success(
+                            HttpStatus.OK, "로그아웃 성공", null);
+
+        } catch (AuthException e) {
+            ErrorCode errorCode = e.getErrorCode();
+            response.setStatus(errorCode.getHttpStatus().value());
+
+            logoutResponse =
+                    RestApiResponse.fail(
+                            errorCode.getHttpStatus(),
+                            errorCode.getMessage(),
+                            errorCode.getName());
+        }
+
+        try {
+            objectMapper.writeValue(response.getWriter(), logoutResponse);
+        } catch (IOException e) {
+            log.error("[Logout] IOException {}", e.getMessage(), e);
+            throw new AuthException(GlobalErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
