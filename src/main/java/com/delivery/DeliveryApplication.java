b8e5823e42cca22b01a922276fbc87e4d2d85845
cpfur18 <cpfur18@gmail.com>
2026-07-22 03:33:04 +0900

    [feature] : DeliveryApplication에 @ConfigurationPropertiesScan 어노테이션을 붙여 @ConfigurationProperties 설정 클래스 스캔하여 자동 Bean 등록

--- src/main/java/com/delivery/DeliveryApplication.java
+++ src/main/java/com/delivery/DeliveryApplication.java
@@ -2,10 +2,12 @@ package com.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DeliveryApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DeliveryApplication.class, args);
     }
